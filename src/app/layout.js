b7f4fb4256baf32bc72534d4b8522a6f0254120a
armorxen <giladiao@hotmail.com>
2023-11-23 12:53:49 -0500

    added _components and Footer, Header, and index.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import * as React from 'react';
-import Header from '@/components/Header';
-import Footer from '@/components/Footer';
+import { Header, Footer } from '@/app/_components'
+// import Header from '@/components/Header';
+// import Footer from '@/components/Footer';
 import ThemeRegistry from '@/components/themeregistry/ThemeRegistry';
 
 const RootLayout = ({ children }) => {
