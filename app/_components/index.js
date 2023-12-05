3f659e65550412ea563dd74b80ac24c1a4d69083
armorxen <giladiao@hotmail.com>
2023-12-04 22:48:49 -0500

    added to _components, _helpers, _services, (secure)

--- app/_components/index.js
+++ app/_components/index.js
@@ -1,3 +1,7 @@
 export * from './Footer';
 export * from './Header';
 export * from './Hero';
+export * from './Alert';
+export * from './Nav';
+export * from './NavLink';
+export * from './Spinner';
